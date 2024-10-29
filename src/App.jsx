f0bd8ef75c9634a0023dc8127f6e5a9feabef43e
Anas <anasanchu3004@gmail.com>
2024-10-29 21:30:22 +0530

    admin login is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AdminLoginPage from './Auth/adminLogin';
+
+
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    <div >
+ 
+    <AdminLoginPage />
+
     </div>
   );
 }
